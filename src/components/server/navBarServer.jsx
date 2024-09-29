9c505481b32b1ad96f3b5bb98a56a5f946d9f649
eteen <eteensmash@gmail.com>
2024-09-28 17:19:28 -0700

    responsive styling added to btn and image in navBar

--- src/components/server/navBarServer.jsx
+++ src/components/server/navBarServer.jsx
@@ -8,7 +8,7 @@ export default function NavBarServer() {
   return (
     <nav className="bg-black flex justify-between items-center league-spartan w-full h-16 mdNavBarHeight xl:px-4 2xl:px-16 overflow-hidden">
       <Link href="/">
-        <div className="relative w-40 h-40 right-4 md:w-48 md:h-48 md:right-6 lg:w-60 lg:h-60 lg:right-7 xl:w-72 xl:h-72 flex ">
+        <div className="relative w-40 h-40 right-4 md:w-48 md:h-48 md:right-6 lg:w-60 lg:h-60 lg:right-7 xl:w-72 xl:h-72 xl:right-12 flex ">
           <Image
             src="/teletaLogo.svg"
             alt="Teleta Development logo linking to homepage"
@@ -22,10 +22,7 @@ export default function NavBarServer() {
         <ul>
           <li className="pr-5">
             <Link href="/">
-              <button
-                className="bg-white text-black text-sm py-1 px-2.5 md:text-base lg:text-xl lg:py-2 lg:px-4 rounded-full tracking-tight"
-                aria-label="Contact Teleta Development"
-              >
+              <button className="bg-white text-black text-sm py-1 px-2.5 md:text-base lg:text-xl lg:py-2 lg:px-4 xl:text-2xl rounded-full tracking-tight" aria-label="Contact Teleta Development">
                 Contact
               </button>
             </Link>{" "}
