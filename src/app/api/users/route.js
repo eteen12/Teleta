06ae33a56178b552de6f4bedb45a7c8fa26f0b66
eteen <eteensmash@gmail.com>
2024-09-30 16:28:40 -0700

    route added, and tested, works with mongo

---
+++ src/app/api/users/route.js
@@ -0,0 +1,34 @@
+import prisma from "@/lib/prismaClient";
+
+export async function POST(request) {
+  const { name, email, message } = await request.json();
+
+  try {
+    const newUser = await prisma.storeUser.create({
+      data: {
+        name,
+        email,
+        message,
+      },
+    });
+
+    return new Response(JSON.stringify(newUser), {
+      status: 201,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    return new Response(JSON.stringify(error), {
+      status: 500,
+      headers: { "Contnent-Type": "application/json" },
+    });
+  }
+}
+
+export async function GET() {
+  const users = await prisma.storeUser.findMany();
+
+  return new Response(JSON.stringify(users), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+}   
