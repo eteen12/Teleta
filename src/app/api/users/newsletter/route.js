aac4019511f99f74f2df6feecb79014c17fdfce6
eteen <eteensmash@gmail.com>
2024-10-02 14:24:46 -0700

    added email validation inside of newsletter

--- src/app/api/users/newsletter/route.js
+++ src/app/api/users/newsletter/route.js
@@ -2,6 +2,14 @@ import prisma from "@/lib/prismaClient";
 
 export async function POST(request) {
   const { email } = await request.json();
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  if (!emailRegex.test(email)) {
+    return new Response(JSON.stringify({ error: "Invalid email format" }), {
+      status: 400,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
 
   try {
     const newSubscriber = await prisma.storeSubscriber.create({
