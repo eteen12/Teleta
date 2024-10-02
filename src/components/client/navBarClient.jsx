710528dc4cfead6f52f4eb3ddb39e4ea6dbee16f
eteen <eteensmash@gmail.com>
2024-10-01 20:32:06 -0700

    seperated server side and client side navbar logic and seperated files into server and client

--- src/components/client/navBarClient.jsx
+++ src/components/client/navBarClient.jsx
@@ -1,11 +1,11 @@
 "use client";
 import React from "react";
-import Link from "next/link";
-import Image from "next/image";
 import { navHandler } from "../../hooks/navHandler.js";
+import NavBarServer from "/src/components/server/navBarServer.jsx";
+
 import "/src/app/globals.css";
 
-export default function NavBarServer() {
+export default function NavBarClient() {
   const { scrollingDown, shadow, background } = navHandler();
   return (
     <nav
@@ -14,32 +14,7 @@ export default function NavBarServer() {
       }`}
       style={{ background, boxShadow: shadow }}
     >
-      <Link href="/">
-        <div className="relative w-40 h-40 right-4 md:w-48 md:h-48 md:right-6 lg:w-60 lg:h-60 lg:right-7 xl:w-72 xl:h-72 xl:right-12 flex ">
-          <Image
-            src="/teletaLogo.svg"
-            alt="Teleta Development logo linking to homepage"
-            className="object-contain"
-            priority
-            fill
-          />
-        </div>
-      </Link>
-      <div>
-        <ul>
-          <li className="pr-5">
-            <Link href="#contact-form">
-              <button
-                className="teletaYellowBg text-black text-sm py-1 px-2.5 md:text-base lg:text-xl lg:py-2 lg:px-4 xl:text-2xl rounded-full tracking-tight league-spartan"
-                aria-label="Contact Teleta Development"
-                style={{ "--font-weight": "800" }}
-              >
-                Contact
-              </button>
-            </Link>{" "}
-          </li>
-        </ul>
-      </div>
+      <NavBarServer />
     </nav>
   );
 }
---
+++ src/components/server/navBarServer.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function NavBarServer() {
+  return (
+    <div className="flex justify-between items-center fixed w-full h-16 z-50 transition-all duration-700">
+      <Link href="/">
+        <div className="relative w-40 h-40 right-4 md:w-48 md:h-48 md:right-6 lg:w-60 lg:h-60 lg:right-7 xl:w-72 xl:h-72 xl:right-12 flex ">
+          <Image
+            src="/teletaLogo.svg"
+            alt="Teleta Development logo linking to homepage"
+            className="object-contain"
+            priority
+            fill
+          />
+        </div>
+      </Link>
+      <div>
+        <ul>
+          <li className="pr-5">
+            <Link href="#contact-form">
+              <button
+                className="teletaYellowBg text-black text-sm py-1 px-2.5 md:text-base lg:text-xl lg:py-2 lg:px-4 xl:text-2xl rounded-full tracking-tight league-spartan"
+                aria-label="Contact Teleta Development"
+                style={{ "--font-weight": "800" }}
+              >
+                Contact
+              </button>
+            </Link>{" "}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
