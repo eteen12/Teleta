d67d09345ca29525f751f3278c181e6c54f6c039
eteen <eteensmash@gmail.com>
2024-10-02 18:59:19 -0700

    packages updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,26 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
-
-export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
-];
+export default {
+  overrides: [
+    {
+      files: ["**/*.{js,mjs,cjs,jsx}"],
+      languageOptions: {
+        globals: {
+          ...globals.browser,
+          ...globals.node,
+        },
+      },
+      plugins: {
+        js: pluginJs,
+        react: pluginReact,
+      },
+      rules: {
+      
+        "react/react-in-jsx-scope": "off",
+      },
+    },
+  ],
+  ...pluginJs.configs.recommended,
+  ...pluginReact.configs.flat.recommended,
+};
